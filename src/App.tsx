a98655e5dd4aaf565b26e9fca27ea9eb974841d8
Lila Papazian <lilapapazian@gmail.com>
2024-11-15 23:05:59 -0300

    add semantic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,168 @@
 function App() {
-
   return (
     <>
-     <h1 className="text-3xl">Hello, world!</h1>
+      <header>
+        <nav>
+          <span>Logo</span>
+          <ul>
+            <li>
+              <a href='#'>Live</a>
+            </li>
+            <li>
+              <a href='#'>Push</a>
+            </li>
+            <li>
+              <a href='#'>Move</a>
+            </li>
+            <li>
+              <a href='#'>Note</a>
+            </li>
+            <li>
+              <a href='#'>Link</a>
+            </li>
+            <li>
+              <a href='#'>Shop</a>
+            </li>
+            <li>
+              <a href='#'>Packs</a>
+            </li>
+            <li>
+              <a href='#'>Help</a>
+            </li>
+            <li>
+              <a href='#'>More +</a>
+            </li>
+            <li>
+              <a href='#'>Try Live 12 for free</a>
+            </li>
+            <li>
+              <a href='#'>Log in or register</a>
+            </li>
+          </ul>
+        </nav>
+        <nav>
+          <ul>
+            <li>
+              <a href='#'>About</a>
+            </li>
+            <li>
+              <a href='#'>Jobs</a>
+            </li>
+            <li>
+              <a href='#'>Apprenticeships</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div>Hero section</div>
+        <section>
+          <h2>
+            We make <a>Live</a>, <a>Push</a>, <a>Note</a> and <a>Link</a> —
+            unique software and hardware for music creation and performance.
+            With these products, our community of users creates amazing things.
+          </h2>
+          <p>
+            Ableton was founded in 1999 and released the first version of Live
+            in 2001. Our products are used by a community of dedicated
+            musicians, sound designers, and artists from across the world.
+          </p>
+        </section>
+        <section>
+          <h2>
+            Making music isn’t easy. It takes time, effort, and learning. But
+            when you’re in the flow, it’s incredibly rewarding.
+          </h2>
+          <p>
+            We feel the same way about making Ableton products. The driving
+            force behind Ableton is our passion for what we make, and the people
+            we make it for.
+          </p>
+        </section>
+        <section>
+          <h2>
+            We are more than 350 people from 30 different countries divided
+            between our headquarters in Berlin and our offices in Los Angeles
+            and Tokyo.
+          </h2>
+          <p>
+            Most of us are active musicians, producers, and DJs, and many of us
+            use Live and Push every day. We come from a wide range of cultural
+            and professional backgrounds. Some of us have PhDs, some are
+            self-taught, and most of us are somewhere in between. What connects
+            us is the shared belief that each of us has the skills and knowledge
+            to contribute to something big: helping to shape the future of music
+            culture.
+          </p>
+        </section>
+        <section>
+          <h2>
+            We believe it takes focus to create truly outstanding instruments.
+            We only work on a few products and we strive to make them great.
+          </h2>
+          <p>
+            Rather than having a one-size-fits-all process, we try to give our
+            people what they need to work their magic and grow. We’ve learned
+            that achieving the best results comes from building teams that are
+            richly diverse, and thus able to explore problems from a wider set
+            of perspectives. We don’t always agree with each other, but opinion
+            and debate are valued and openly encouraged.
+          </p>
+        </section>
+        <section>
+          <h2>
+            We’re passionate about what we do, but we’re equally passionate
+            about improving who we are.
+          </h2>
+          <p>
+            We work hard to foster an environment where people can grow both
+            personally and professionally, and we strive to create a wealth of
+            opportunities to learn from and with each other.
+          </p>
+          <p>
+            Alongside an internal training program, employees are actively
+            supported in acquiring new knowledge and skills, and coached on
+            applying these in their daily work. In addition, staff-organized
+            development and music salons are a chance to discuss new
+            technologies, production techniques and best practices.
+          </p>
+        </section>
+        <section>
+          <h2>
+            We want our employees to love it here. Since we’re looking for
+            exceptional talent from around the world, we will do everything we
+            can to make your transition as easy as possible.
+          </h2>
+          <p>
+            If you're joining us in Berlin, we'll help with relocation and
+            paperwork. We’ll even provide you with free German or English
+            lessons. Plus, working in Germany means you can expect comprehensive
+            health insurance for you and your family, as well as generous
+            maternity and paternity leave. Office hours are flexible, but it’s
+            not all work; we have several company and team outings throughout
+            the year as well as a variety of fun, informal small-group
+            activities.
+          </p>
+        </section>
+      </main>
+      <footer>
+        <div>Socials</div>
+        <div>Education</div>
+        <div>Sign up to our newsletter</div>
+        <div>Community</div>
+        <div>Language and Location</div>
+        <ul>
+          <li>Contact Us</li>
+          <li>Press Resources</li>
+          <li>Legal Info</li>
+          <li>Privacy Policy</li>
+          <li>Cookie Settings</li>
+          <li>Imprint</li>
+        </ul>
+        <div>Made in Berlin</div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
